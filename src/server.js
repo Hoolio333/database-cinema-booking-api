5bddf52ebddf5aabb0923c7ac05a1d438efa81d6
Chris Hyde <c_hyde333@hotmail.com>
2022-06-09 16:01:23 +0100

    Core Criteria Done

--- src/server.js
+++ src/server.js
@@ -15,7 +15,11 @@ app.use(express.urlencoded({ extended: true }));
 // Tell express to use your routers here
 const customerRouter = require("./routers/customer");
 const moviesRouter = require("./routers/movies");
+const screensRouter = require("./routers/screens");
+const ticketsRouter = require("./routers/tickets");
 app.use("/customer", customerRouter);
 app.use("/movies", moviesRouter);
+app.use("/screens", screensRouter);
+app.use("/tickets", ticketsRouter);
 
 module.exports = app;
